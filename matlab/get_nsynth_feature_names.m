2c70fb25b879d7acc2442653511666c578e9c935
Achim Leydecker <achim@R400>
2015-02-19 16:29:25 +0100

    removed old version / matlab files

--- matlab/get_nsynth_feature_names.m
+++
@@ -1,6 +0,0 @@
-function features = get_nsynth_feature_names(nsynth_nifti_dir)
-	features = dir(fullfile(nsynth_nifti_dir, '*.nii'));
-	features = {features(:).name};
-	features = regexprep(features(:), '_pFgA_z.nii', '');
-	features = regexprep(features(:), '_pAgF_z.nii', '');
-end
