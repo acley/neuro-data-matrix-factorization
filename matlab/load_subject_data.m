aac515327de85aad207c4284a70ade88ada1b6e6
Achim Leydecker <achim@R400>
2015-03-03 15:53:55 +0100

    remove folder structure

--- matlab/load_subject_data.m
+++ matlab/load_subject_data.m
@@ -7,7 +7,7 @@ function subject_data = load_subject_data()
 	num_subjects = length(subjs);
 	
 	linearkernel = 1;
-	for isubj = 1:num_subjects
+	for isubj = 1:3%num_subjects
 		fprintf('Loading Subject %d\n',isubj)
 		subject_data{isubj} = load(fullfile(subject_data_dir,subjs(isubj).name), 'conditions', 'K_lin', 'dat');
 		subject_data{isubj}.K = subject_data{isubj}.K_lin;
--- matlab/set_paths.m
+++ matlab/set_paths.m
@@ -1,47 +1,62 @@
-% setup paths for fix_single_condition_contrast experiment
-
-rng('default'); % fix random number generator. necessary?
-
+rng('default');
 
 if ispc == 1
-	base_dir = 'D:\home\achim\Masterprojekt\working';
-	subject_data_dir = 'D:\home\achim\Masterprojekt\working\input_subject_data';
-	nsynth_pattern_dir = 'D:\home\achim\Masterprojekt\working\input_nsynth_pattern';
-	functional_dir = 'D:\home\achim\Masterprojekt\ORIGINAL\functional';
-	structural_dir = 'D:\home\achim\Masterprojekt\ORIGINAL\structural';
-	logfile_dir = 'D:\home\achim\Masterprojekt\ORIGINAL\logfiles';
-	nsynth_reference_file_path = 'D:\home\achim\Masterprojekt\ORIGINAL\functional\ability_pAgF_z.nii';
-	preprocessed_data_dir = 'D:\home\achim\Masterprojekt\working\preprocessed_data';
 else
 	subject_data_dir = '/home/achim/Data/sequrea/working/masterprojekt/input_subject_data';
 	base_dir = '/home/achim/Data/sequrea/working/projekt';
-	% nsynth paths
 	config.working_root_dir = '/home/achim/Data/sequrea/working/masterprojekt';
 	nsynth_pattern_dir = fullfile(config.working_root_dir, 'input_nsynth_pattern');
 end
 
 experiment_name = 'fixed_viewing_condition';
 
-cca_output_base_dir = fullfile(base_dir, 'cca_output');
-cca_output_dir = fullfile(cca_output_base_dir, [experiment_name, '_cv']);
-if exist(cca_output_dir) ~= 7
-	mkdir(cca_output_dir);
-end
+base_dir = fullfile(base_dir, experiment_name);
 
-cca_output_averaged_dir = fullfile(cca_output_base_dir, [experiment_name, '_averaged']);
-if exist(cca_output_averaged_dir) ~= 7
-	mkdir(cca_output_averaged_dir);
-end
 
-cca_output_random_dir = fullfile(cca_output_base_dir, [experiment_name, '_random']);
-if exist(cca_output_random_dir) ~= 7
-	mkdir(cca_output_random_dir);
-end
+%% setup paths for fix_single_condition_contrast experiment
 
-random_data_path = fullfile(cca_output_base_dir, 'random_weights.mat');
+%rng('default'); % fix random number generator. necessary?
 
-visualisation_base_dir = fullfile(base_dir, 'cca_output_visualisations');
-cca_visualisation_dir = fullfile(visualisation_base_dir, [experiment_name, '_cv']);
-if exist(cca_visualisation_dir) ~= 7
-	mkdir(cca_visualisation_dir);
-end
+
+%if ispc == 1
+%	base_dir = 'D:\home\achim\Masterprojekt\working';
+%	subject_data_dir = 'D:\home\achim\Masterprojekt\working\input_subject_data';
+%	nsynth_pattern_dir = 'D:\home\achim\Masterprojekt\working\input_nsynth_pattern';
+%	functional_dir = 'D:\home\achim\Masterprojekt\ORIGINAL\functional';
+%	structural_dir = 'D:\home\achim\Masterprojekt\ORIGINAL\structural';
+%	logfile_dir = 'D:\home\achim\Masterprojekt\ORIGINAL\logfiles';
+%	nsynth_reference_file_path = 'D:\home\achim\Masterprojekt\ORIGINAL\functional\ability_pAgF_z.nii';
+%	preprocessed_data_dir = 'D:\home\achim\Masterprojekt\working\preprocessed_data';
+%else
+%	subject_data_dir = '/home/achim/Data/sequrea/working/masterprojekt/input_subject_data';
+%	base_dir = '/home/achim/Data/sequrea/working/projekt';
+%	% nsynth paths
+%	config.working_root_dir = '/home/achim/Data/sequrea/working/masterprojekt';
+%	nsynth_pattern_dir = fullfile(config.working_root_dir, 'input_nsynth_pattern');
+%end
+
+%experiment_name = 'fixed_viewing_condition';
+
+%cca_output_base_dir = fullfile(base_dir, 'cca_output');
+%cca_output_dir = fullfile(cca_output_base_dir, [experiment_name, '_cv']);
+%if exist(cca_output_dir) ~= 7
+%	mkdir(cca_output_dir);
+%end
+
+%cca_output_averaged_dir = fullfile(cca_output_base_dir, [experiment_name, '_averaged']);
+%if exist(cca_output_averaged_dir) ~= 7
+%	mkdir(cca_output_averaged_dir);
+%end
+
+%%cca_output_random_dir = fullfile(cca_output_base_dir, [experiment_name, '_random']);
+%%if exist(cca_output_random_dir) ~= 7
+%%	mkdir(cca_output_random_dir);
+%%end
+
+%%random_data_path = fullfile(cca_output_base_dir, 'random_weights.mat');
+
+%visualisation_base_dir = fullfile(base_dir, 'cca_output_visualisations');
+%cca_visualisation_dir = fullfile(visualisation_base_dir, [experiment_name, '_cv']);
+%if exist(cca_visualisation_dir) ~= 7
+%	mkdir(cca_visualisation_dir);
+%end
